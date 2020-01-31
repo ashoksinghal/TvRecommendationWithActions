6611b9d015abf7954985a92b52dd6efc91d336e5
raghav sharma <raghav.sharma@telekom-digital.com>
2020-01-31 17:12:17 +0530

    Commit

--- app/src/main/java/com/devrel/android/fitactions/recommendation/MainActivity.kt
+++ app/src/main/java/com/devrel/android/fitactions/recommendation/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.devrel.android.fitactions.recommendation
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.devrel.android.fitactions.R
 import com.example.myapplication.Channel
 import com.google.gson.Gson
@@ -25,15 +29,59 @@ class MainActivity : AppCompatActivity() {
 
         GlobalScope.launch(Dispatchers.IO) {
             try {
-                val string = URL("http://10.178.23.94:1337/channels/recommend/1").readText()
+                val string = URL("http://192.168.20.141:1337/channels/recommend/1").readText()
+                val string2 = URL("http://192.168.20.141:1337/channels/recommend/high/1").readText()
+
 
                 val result = Gson().fromJson<List<Channel>>(
                     string,
                     type
                 )
+
+                val result2 = Gson().fromJson<List<Channel>>(
+                    string2,
+                    type
+                )
+
                 GlobalScope.launch(Dispatchers.Main) {
-                    progress_bar.visibility = View.GONE
+                    recyclerview.addItemDecoration(
+                        DividerItemDecoration(
+                            recyclerview.context,
+                            DividerItemDecoration.VERTICAL
+                        )
+                    )
+
+                    recyclerview.addItemDecoration(
+                        DividerItemDecoration(
+                            recyclerview.context,
+                            DividerItemDecoration.HORIZONTAL
+                        )
+                    )
+
+
+                    recyclerview2.addItemDecoration(
+                        DividerItemDecoration(
+                            recyclerview2.context,
+                            DividerItemDecoration.VERTICAL
+                        )
+                    )
+
+                    recyclerview2.addItemDecoration(
+                        DividerItemDecoration(
+                            recyclerview2.context,
+                            DividerItemDecoration.HORIZONTAL
+                        )
+                    )
+
+
                     recyclerview.adapter = ChannelAdapter(result)
+                    recyclerview2.adapter = ChannelAdapter(result2)
+
+                    progress_bar.visibility = View.GONE
+                    container1.visibility = View.VISIBLE
+                    container2.visibility = View.VISIBLE
+
+
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
